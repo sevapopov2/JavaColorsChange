76c29fe258e04ae4b9f7d81cf97e61bad26a2614
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-30 00:25:42 +0300

    Added an example animal constructor

---
+++ src/main/java/AnimalsImplementation/Animal.java
@@ -0,0 +1,16 @@
+package AnimalsImplementation;
+
+public class Animal {
+    //Constructor variables deffinition.
+    String name;
+    String height;
+    String weight;
+    String age;
+
+    public Animal(String name, String height, String weight, String age) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+    }
+}
