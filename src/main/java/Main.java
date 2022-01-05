239f431dfd6a2151fe82c3a092b7bc7ad56af693
Vsevolod Popov <sevapopov13@gmail.com>
2022-01-05 21:45:29 +0300

    Added an interface to cat's class and removed it from main method. Code is optimized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 import animals.Cat;
-import colors.TextColorChange;
 
-public class Main implements TextColorChange{
+public class Main {
     public static void main(String[] args) {
         Cat murka = new Cat(250, 3700, 1);
-        murka.writeColoredText(RED_COLOR, "I will destroy you! Because I am Murka!");
+        murka.hideFromDaddy(true);
     }
 }
--- src/main/java/animals/Cat.java
+++ src/main/java/animals/Cat.java
@@ -1,9 +1,23 @@
 package animals;
 
-public class Cat extends Animal {
+import colors.TextColorChange;
+
+public class Cat extends Animal implements TextColorChange{
 
     public Cat(int height, int weight, int age) {
         super(height, weight, age);
     }
 
+    public boolean hideFromDaddy(boolean hide) {
+        if (hide) {
+            writeColoredText(BLACK_COLOR, "I hided from him! Hahahaha! I am in a black mask!");
+        } else {
+            writeColoredText(RED_COLOR, "Oh no! He can find me! I am afraid! Aaa!");
+        }
+        return hide;
+    }
+@Override
+public void writeColoredText(String color, String text) {
+    System.out.println(color + text + COLOR_RESET);
+}    
 }
