a49f136d7fd434fc22e8d2fc675e01c1ecfa971d
Vsevolod Popov <sevapopov13@gmail.com>
2022-01-05 21:16:20 +0300

    Removed TextColorMethod from cat's class.

--- src/main/java/animals/Cat.java
+++ src/main/java/animals/Cat.java
@@ -1,15 +1,9 @@
 package animals;
 
-import colors.TextColorChange;
-
-public class Cat extends Animal implements TextColorChange{
+public class Cat extends Animal {
 
     public Cat(int height, int weight, int age) {
         super(height, weight, age);
     }
 
-    @Override
-    public void writeColoredText(String color, String text) {
-        System.out.println(color + text + COLOR_RESET);
-    }
 }
