a8398216401b2deaf2686ed124324cef4cae22ab
Vsevolod Popov <sevapopov13@gmail.com>
2022-01-05 19:28:51 +0300

    made cat class as implement of TextColorChange interface.

--- src/main/java/animals/Cat.java
+++ src/main/java/animals/Cat.java
@@ -1,6 +1,8 @@
 package animals;
 
-public class Cat extends Animal {
+import colors.TextColorChange;
+
+public class Cat extends Animal implements TextColorChange{
 
     public Cat(int height, int weight, int age) {
         super(height, weight, age);
