bee6affc7acd35990c9652898e1568408fd48c18
Vsevolod Popov <sevapopov13@gmail.com>
2022-01-07 13:32:12 +0300

    Added ColoredTextPrinting execution to main class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import animals.Cat;
 public class Main {
     public static void main(String[] args) {
         Cat murka = new Cat(250, 3700, 1);
+        ColoredTextprinting secondExample = new ColoredTextprinting();
         murka.hideFromHuman(true);
+        murka.hideFromHuman(false);
+        secondExample.printRandomText();
     }
 }       
